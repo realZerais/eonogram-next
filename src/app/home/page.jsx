197463433cd7ffc14b72b82b96873e5fd75768b9
Dave Siarez <davesiarez18@gmail.com>
2025-06-20 14:01:56 +0800

    posts design init

---
+++ src/app/home/page.jsx
@@ -0,0 +1,24 @@
+import { createClient } from '@/utils/supabase/server';
+import { Post } from '@/components/Posts/Post';
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: posts } = await supabase.from("posts").select();
+
+  // return <pre>{JSON.stringify(posts, null, 2)}</pre>
+
+
+  
+  return(
+    <div className='w-2/3 p-2 h-full'>
+      
+      <h2 className='font-semibold'>Posts</h2>
+  
+
+      {posts.map((post) =>(
+        <Post post={post} key={post.id}></Post>
+      ))}
+
+    </div>
+  )
+}
--- src/app/posts/page.jsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/app/utils/supabase/server';
-
-export default async function Posts() {
-  const supabase = await createClient();
-  const { data: posts } = await supabase.from("posts").select();
-
-  return <pre>{JSON.stringify(posts, null, 2)}</pre>
-}
